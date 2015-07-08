e56a18fe21ca18a84fe9411463d4b2742ab7185d
musaichi <musaichich@gmail.com>
2015-07-08 19:36:28 +0900

    KGI.php なんか色々編集しました。

--- src/musaichi/KGI.php
+++ src/musaichi/KGI.php
@@ -6,5 +6,14 @@ use pocketmine\event\player\playerKillEvent;
 use pocketmine\item\Item;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\player;
+use pocketmine\utils\textFormat;
 
 class KGI extends pluginBase{
+
+  public function onEnable(){
+   $this->getLogger()->info("KillGiveItem now loading...made by musaichiJP")
+  }
+  public function onLoad(){
+   $this->getLogger()->info("done!Please enjoy this plugin life!")
+  }
+  public function PlayerKillEvent
