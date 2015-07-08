f433935a9e591954538e1b60a89ca2c254495d9c
musaichi <musaichich@gmail.com>
2015-07-08 15:12:20 +0900

    KGI.php use文の一部を編集しました。

--- src/musaichi/KGI.php
+++ src/musaichi/KGI.php
@@ -1,1 +1,10 @@
 <?php
+namespace KGI
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\playerKillEvent;
+use pocketmine\item\Item;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\player;
+
+class KGI extends pluginBase{
