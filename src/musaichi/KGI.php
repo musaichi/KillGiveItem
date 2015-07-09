367c9dc25b32324bcd837991ab5bdf55c4237d89
musaichi <musaichich@gmail.com>
2015-07-09 09:41:28 +0900

    KGI.php config設定

--- src/musaichi/KGI.php
+++ src/musaichi/KGI.php
@@ -6,10 +6,18 @@ use pocketmine\item\Item;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\player;
 use pocketmine\utils\textFormat;
+use pocketmine\utils\Config;
 
 class KGI extends pluginBase{
 
   public function onEnable(){
+   if(!file_exists($this->getDataFolder())){
+    mkdir($this->getDataFolder(), 0744, true);//なければフォルダを作成
+}
+   $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML array(
+        "test" => "data",
+        "データ名(キー)" => "値",
+));
    $this->getLogger()->info("KillGiveItem now loading...made by musaichiJP")
   }
   public function onLoad(){
@@ -17,7 +25,7 @@ class KGI extends pluginBase{
   }
   public function PlayerDeathEvent(PlayerDeathEvent $event){
    if($event instanceof PlayerDeathEvent){
-    $item = 
+    $item = Item::get(1, 0, 1)
     $killer = $event->getKiller();//ころしたひと
     $killer->sendPopup("you are winner!gived item $item ")
     
